f2e57dd756a0a2c35ff452134dc0b91023873a6d
anandanshul <anshulanand194@gmail.com>
2026-03-12 07:39:16 +0000

    added inspect command

---
+++ command_inspect.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func commandInspect(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("not valid Pokemon")
+	}
+	name := args[0]
+	pokemon, exists := cfg.caughtPokemon[name]
+	if !exists {
+		return errors.New("pokemon not caught")
+	}
+	fmt.Println("Name:", pokemon.Name)
+	fmt.Println("Height:", pokemon.Height)
+	fmt.Println("Weight:", pokemon.Weight)
+	fmt.Println("Stats:")
+	for _, stat := range pokemon.Stats {
+		fmt.Printf("  -%s: %v\n", stat.Stat.Name, stat.BaseStat)
+	}
+	fmt.Println("Types:")
+	for _, typeInfo := range pokemon.Types {
+		fmt.Println("  -", typeInfo.Type.Name)
+	}
+	return nil
+
+}
